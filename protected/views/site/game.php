14fbb5e17524b3ef743c9b050960158996ac5055
Vincen Wen <vincen.wen@samesamechina.com>
2015-07-27 15:46:50 +0800

    no message

--- protected/views/site/game.php
+++ protected/views/site/game.php
@@ -1,4 +1,19 @@
+<div class="scoreList">
+	<div class="scoreList_title"><span>头像</span><span>排名</span><span>玩家昵称</span><span>积分</span></div>
+	<ul>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>1</span><span>vic</span><span>100000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>2</span><span>jack qiuith</span><span>90000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>3</span><span>v</span><span>6000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>4</span><span>愤怒的文子</span><span>2000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>5</span><span>v</span><span>100000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>6</span><span>v</span><span>90000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>7</span><span>v</span><span>6000</span></li>
+		<li><span><img src="/angelfish/imgs/a.gif" width="100%" /></span><span>8</span><span>v</span><span>2000</span></li>
+	</ul>
+</div>
 <article id="dramebox">
+	
+
 	<canvas id="canvas"></canvas>
 	<a href="javascript:location.reload();" id="replay">重新挑战</a>
 </article>
