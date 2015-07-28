c77e648dd0beaa57d25caa97f7126c31eeb1fec8
Demon Zhang <demon.zhang@samesamechina.com>
2015-07-28 17:02:59 +0800

    no message

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -61,6 +61,43 @@ class ApiController extends Controller
 		Yii::app()->end();
 	}
 
+	public function actionJoinTeam()
+	{
+		if (!isset($_SESSION['weixin_info_id'])) {
+			print json_encode(array('code' => 0, 'msg' => '未登录'));
+			Yii::app()->end();
+		}
+
+		$tag = false;
+		$id = isset($_POST['id']) ? intval($_POST['id']) : $tag = true;
+		if ($tag) {
+			print json_encode(array('code' => 2, 'msg' => '参数错误'));
+			Yii::app()->end();
+		}
+		$db = Yii::app()->db;
+
+		$sql = "SELECT id FROM `same_game_team` WHERE `uid` = '". intval($_SESSION['weixin_info_id']).
+			 "' or `fid` = '". intval($_SESSION['weixin_info_id'])."'";
+		$rs = $db->createCommand($sql)->select()->queryScalar();
+		if ($rs) {
+			print json_encode(array('code' => 3, 'msg' => '已经有战队了'));
+			Yii::app()->end();
+		}
+
+		$sql = "SELECT * FROM `same_game_team` WHERE `id` = '". $id. "'";
+		$rs = $db->createCommand($sql)->select()->queryRow();
+		if ($rs['fid']) {
+			print json_encode(array('code' => 4, 'msg' => '战队已满'));
+			Yii::app()->end();
+		}
+
+		$sql = "UPDATE `same_game_team` SET `fid` = '". intval($_SESSION['weixin_info_id']) . "' WHERE `id` = '".
+		     $id ."'";
+		$db->createCommand($sql)->execute();
+		print json_encode(array('code' => 1, 'msg' => '加入成功'));
+		Yii::app()->end();
+	}
+
 	public function actionSubScore($type)
 	{
 		if (!isset($_SESSION['weixin_info_id'])) {
@@ -129,10 +166,16 @@ class ApiController extends Controller
 		switch ($type) {
 			case '1':
 				$sql = "SELECT * FROM `same_weixin_info` ORDER BY score DESC LIMIT 10";
+				$scoreList = $db->createCommand($sql)->select()->queryAll();
+				$sql = "SELECT count(*) FROM `same_weixin_info` WHERE score > (SELECT score FROM `same_weixin_info` WHERE id = '". intval($_SESSION['weixin_info_id']) ."')";
+				$num = $db->createCommand($sql)->select()->queryScalar();
 				break;
 			
 			case '2':
 				$sql = "SELECT uscore+fscore as score,name FROM `same_game_team` ORDER BY score DESC LIMIT 10";
+				$scoreList = $db->createCommand($sql)->select()->queryAll();
+				$sql = "SELECT count(*) FROM `same_game_team` WHERE uscore+fscore > (SELECT uscore+fscore FROM `same_weixin_info` WHERE uid = '". intval($_SESSION['weixin_info_id']) ."' or fid = '". intval($_SESSION['weixin_info_id']) ."')";
+				$num = $db->createCommand($sql)->select()->queryScalar();
 				break;
 
 			default:
@@ -140,8 +183,8 @@ class ApiController extends Controller
 				Yii::app()->end();
 				break;
 		}
-		$scoreList = $db->createCommand($sql)->select()->queryAll();
-		print json_encode(array('code' => 1, 'msg' => $scoreList));
+		
+		print json_encode(array('code' => 1, 'msg' => $scoreList, 'ranking'=>$num+1));
 		Yii::app()->end();
 	}
 }
