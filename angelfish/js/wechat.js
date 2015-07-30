d7ef3dda6aae61a3341d04d40648c8709eff80f9
Vincen Wen <vincen.wen@samesamechina.com>
2015-07-30 16:17:30 +0800

    no message

--- angelfish/js/wechat.js
+++ angelfish/js/wechat.js
@@ -30,7 +30,8 @@ function wechatFun(){
 
 
 function wechatShare(){
-
+    alert(wechatParameter._appid);
+    alert(wechatParameter._sign);
   wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: wechatParameter._appid, // 必填，公众号的唯一标识
