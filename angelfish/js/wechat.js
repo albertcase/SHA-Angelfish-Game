eee87433f46577aba6cce656262922a0cf766149
Vincen Wen <vincen.wen@samesamechina.com>
2015-07-30 16:25:41 +0800

    no message

--- angelfish/js/wechat.js
+++ angelfish/js/wechat.js
@@ -13,7 +13,7 @@ var shareData = {
 function wechatFun(){
     $.ajax({
         type: "POST",
-        url: "/weixin/jssdk/url/" + window.location.href,
+        url: "/weixin/jssdk/url/" + encodeURIComponent(window.location.href),
         dataType:"json"
     }).done(function(data){
         wechatParameter = {
