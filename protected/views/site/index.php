3dc5e2524717205d71f40e8653371292f3f76fdd
Vincen Wen <vincen.wen@samesamechina.com>
2015-07-30 19:00:32 +0800

    no message

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,36 +1,40 @@
-
-<div id="rule">
-	<img src="/angelfish/imgs/rule.jpg" width="100%" />
-	<a href="javascript:;" class="back">
-		<img src="/angelfish/imgs/backBtn.png" width="100%" />
-	</a>
+<div id="loading">
+	<div class="loadingImg">
+		<img src="/angelfish/imgs/loading.png" width="100%" />
+		<span>-</span>
+	</div>
 </div>
 
 <article id="dramebox">
-	<div id="loading"></div>
+	<div id="rule">
+		<img src="" sourcesrc="/angelfish/imgs/rule.jpg" width="100%" />
+		<a href="javascript:;" class="back">
+			<img src="" sourcesrc="/angelfish/imgs/backBtn.png" width="100%" />
+		</a>
+	</div>
 
 	<section class="container">
-		<img src="/angelfish/imgs/home_bg.jpg" width="100%" />
+		<img src="" sourcesrc="/angelfish/imgs/home_bg.jpg" width="100%" />
 		<div class="menu">
 			<div class="ball1">
-				<img src="/angelfish/imgs/ball1.png" width="100%" />
+				<img src="" sourcesrc="/angelfish/imgs/ball1.png" width="100%" />
 			</div>
 			<div class="ball2">
-				<img src="/angelfish/imgs/ball2.png" width="100%" />
+				<img src="" sourcesrc="/angelfish/imgs/ball2.png" width="100%" />
 			</div>
 			<a href="/site/game/type/1" class="startP-1" title="个人赛">
-				<img src="/angelfish/imgs/1p.png" width="100%" />
+				<img src="" sourcesrc="/angelfish/imgs/1p.png" width="100%" />
 			</a>
 			<a href="/site/game/type/2" class="startP-2" title="双打赛">
-				<img src="/angelfish/imgs/2p.png" width="100%" />
+				<img src="" sourcesrc="/angelfish/imgs/2p.png" width="100%" />
 			</a>
 		</div>
 
 		<a href="/site/result" class="playersRanking_link">
-			<img src="/angelfish/imgs/playersRanking.png" width="100%" />
+			<img src="" sourcesrc="/angelfish/imgs/playersRanking.png" width="100%" />
 		</a>
 		<a href="javascript:;" class="rule_link">
-			<img src="/angelfish/imgs/rule_text.png" width="100%" />
+			<img src="" sourcesrc="/angelfish/imgs/rule_text.png" width="100%" />
 		</a>
 	</section>
 </article>
