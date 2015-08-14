8a2cac4aba4188ed23fb9906448ec3769e81ca98
Vincen Wen <vincen.wen@samesamechina.com>
2015-08-14 11:13:41 +0800

    no message

--- angelfish/js/interface.js
+++ angelfish/js/interface.js
@@ -41,6 +41,7 @@
     				var gTeamId = data.team;
 	    			var gt = getType();
 					if(gt == 2 && gTeamId){
+						alert(6);
 						shareData = {
 							title: 'a球王就是你，'+data.teamname+'邀你加入网球大师赛',
 					   		desc: '您的好友邀您加入TA的'+data.teamname+'战队，一起赢取大师杯门票和百瓶葡萄酒。',
